f663d713a9ec2b6e5b9a8064ff2f8af9a7f2b4ef
Arnaud Loonstra <arnaud@sphaero.org>
2015-03-08 12:25:22 +0100

    initial commit

---
+++ gnc_import.py
@@ -0,0 +1,148 @@
+from gnucash import Session, Account, Transaction, Split, GncNumeric, gnucash_core
+import mt940
+from mt940 import MT940
+from decimal import Decimal
+import sys
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Import a MT940 file into Gnucash and match Gnucash accounts to transactions.')
+parser.add_argument('mt940', type=str, help='mt940 file to import from')
+parser.add_argument('gnucash', type=str, help='gnucash file to import to')
+
+args = parser.parse_args()
+print("Parsing mt940 file {0} and Gnucash file {1}".format(args.mt940, args.gnucash))
+
+def gnc_get_child_accounts_dict(account, d=None):
+    """
+    Returns a list of all child accounts of account
+    """
+    if not d:
+        d={}
+    children = account.get_children()
+    if children:
+        for acc in account.get_children():
+            d = gnc_get_child_accounts_dict(acc, d)
+    else:
+        d[account.name] = account
+    return d
+
+def gnc_numeric_from_decimal(decimal_value):
+    """
+    Returns a GncNumeric of a decimal value
+    """
+    sign, digits, exponent = decimal_value.as_tuple()
+ 
+    # convert decimal digits to a fractional numerator
+    # equivlent to
+    # numerator = int(''.join(digits))
+    # but without the wated conversion to string and back,
+    # this is probably the same algorithm int() uses
+    numerator = 0
+    TEN = int(Decimal(0).radix()) # this is always 10
+    numerator_place_value = 1
+    # add each digit to the final value multiplied by the place value
+    # from least significant to most sigificant
+    for i in xrange(len(digits)-1,-1,-1):
+        numerator += digits[i] * numerator_place_value
+        numerator_place_value *= TEN
+
+    if decimal_value.is_signed():
+        numerator = -numerator
+
+    # if the exponent is negative, we use it to set the denominator
+    if exponent < 0 :
+        denominator = TEN ** (-exponent)
+        # if the exponent isn't negative, we bump up the numerator
+        # and set the denominator to 1
+    else:
+        numerator *= TEN ** exponent
+        denominator = 1
+
+    return GncNumeric(numerator, denominator)
+
+def _get_leave_account(acc, acc_dict):
+    children = acc.get_children()
+    print("boe", children)
+    if children:
+        print("yesy")
+        for a in children():
+            print("huh", a.name)
+            acc_dict = _get_leave_account(a, acc_dict)
+    else:
+        acc_dict[acc.name] = acc
+    return acc_dict    
+
+try:
+    session = Session(args.gnucash)
+except gnucash_core.GnuCashBackendException as e:
+    print(e)
+    sys.exit(1)
+
+book = session.get_book()
+currency = book.get_table().lookup("CURRENCY", "EUR")
+ing = book.get_root_account().get_children()[0].get_children()[0].get_children()[0]     
+root_acc = book.get_root_account()
+
+# dict containing all expenses accounts (flat tree)
+child_accounts = gnc_get_child_accounts_dict(root_acc)
+
+#print(gnc_get_child_accounts_dict(expenses))
+
+mt = MT940(args.mt940)
+# this should usually be one statement
+
+transacts = []
+
+def gen_description(description):
+    description = mt940.ing_description(description)
+    ret = ""
+    if description.get('remi'):
+        ret += description['remi']['remittance_info']
+    if description.get('cntp'):
+        if len(ret): ret+= " "
+        ret += description['cntp'].get('name', '')
+    return ret
+
+for statement in mt.statements:
+    #print(statement.information)
+    for transaction in statement.transactions:
+        #print(transaction.booking, transaction.amount, transaction.reference, transaction.account, transaction.description)
+        t = {}
+        t['date'] = transaction.booking
+        t['amount'] = transaction.amount
+        t['desc'] = gen_description(str(transaction.description).replace('\n',''))
+        t['account'] = ""
+        transacts.append(t)
+
+import gnc_urw
+
+try:
+    print(gnc_urw.gnc_urw_edit(transacts, child_accounts))
+    print(transacts)
+
+    for t in transacts:
+        #print gnc_numeric_from_decimal(tamount)
+        
+        trans = Transaction(book)
+        trans.BeginEdit()
+        trans.SetCurrency(currency)
+        split1 = Split(book)
+        split1.SetValue(gnc_numeric_from_decimal(t['amount']))
+        split1.SetAccount(ing)
+        split1.SetParent(trans)
+
+        split2 = Split(book)
+        split2.SetValue(gnc_numeric_from_decimal(t['amount']).neg())
+        split2.SetAccount(t['account'])
+        split2.SetParent(trans)
+            
+        trans.SetDescription(t['desc'])
+        tdate = t['date']
+        trans.SetDate(tdate.day, tdate.month, tdate.year)
+        trans.CommitEdit()
+finally:
+    session.save()
+    session.end()
+    session.destroy()
+
---
+++ gnc_urw.py
@@ -0,0 +1,143 @@
+import urwid
+import sys
+import io
+import time
+
+transactions = [{'desc':'1'},{'desc':'2'},{'desc':'3'},{'desc':'4'},{'desc':'5'},{'desc':'6'},{'desc':'7'}]
+options = {'bla': 'bla', 'cavia':'cavia', 'kantoor':'kantoor', 'kantine':'kantine'}
+
+class UrwStdWriter(io.TextIOWrapper):
+
+    def __init__(self, *args, **kwargs):
+        self.wgt = urwid.Text("")
+        self.oldout = sys.stdout
+        super(UrwStdWriter, self).__init__(io.BytesIO(), sys.stdout.encoding, *args, **kwargs)
+        self.maxrows = 5
+        
+    def capture_stdout(self):
+        self.oldout = sys.stdout
+        sys.stdout = self
+        
+    def release_stdout(self):
+        sys.stdout = self.oldout
+    
+    def __del__(self):
+        self.release_stdout()
+
+    def write(self, data):
+        #if self.wgt.get_text()[0].count('\n') > self.wgt.rows() and len(self.wgt.get_text()):
+        if self.wgt.get_text()[0].count('\n') > self.maxrows:
+            cuttxt = self.wgt.get_text()[0].split('\n')[1:]
+            self.wgt.set_text("%s%s" %(self.wgt.get_text()[0].split('\n',1)[1], data))
+        else:
+            self.wgt.set_text("%s%s" %(self.wgt.get_text()[0], data))
+
+    def get_widget(self):
+        return self.wgt
+
+
+class EditCompletion(urwid.Edit):
+    
+    def __init__(self, options, *args, **kwargs):
+        self.options = options
+        super(EditCompletion, self).__init__(*args, **kwargs)
+
+    def keypress(self, size, key):
+        p = self.edit_pos
+        super(EditCompletion, self).keypress(size, key)
+        
+        if self.valid_char(key):
+            p = self.edit_pos
+            cur = self.get_edit_text()
+            if len(cur) and p:
+                for o in self.options:
+                    if o.startswith(cur[:p]):
+                        self.set_edit_text(o)
+                        break
+            if self.get_edit_text() in self.options:
+                return key
+        else:
+        #elif key=="tab" and self.allow_tab:
+        #    key = " "*(8-(self.edit_pos%8))
+        #    self.insert_text(key)
+            return key
+
+class TransactionListBox(urwid.ListBox):
+    def __init__(self, transactions, account_options, *args, **kwargs):
+        tws = []
+        for t in transactions:
+            tws.append(urwid.Pile((urwid.Text(('title transaction', "[{0}]{1}".format(t.get('amount', 0), t['desc']))), EditCompletion(account_options))))
+        #urwid.Pile()
+        body = urwid.SimpleFocusListWalker(tws)
+        
+        super(TransactionListBox, self).__init__(body)
+        
+    #def keypress(self, size, key):
+    #    if key != 'enter':
+    #        return super(TransactionListBox, self).keypress(size, key)
+    #    self.original_widget = urwid.Text(
+    #        u"Nice to meet you,\n%s.\n\nPress Q to exit." %
+    #        edit.edit_text)
+
+palette = [
+    (None,  'light gray', 'black'),
+    ('heading', 'black', 'light gray'),
+    ('line', 'black', 'light gray'),
+    ('options', 'dark gray', 'black'),
+    ('focus heading', 'white', 'dark red'),
+    ('focus line', 'black', 'dark red'),
+    ('focus options', 'black', 'light gray'),
+    ('selected', 'light gray', 'dark blue'),
+    ('title transaction', 'white', 'dark blue')]
+        
+def on_cancel_clicked(button):
+    raise urwid.ExitMainLoop()
+
+def gnc_urw_edit(transactions, options):
+
+    def unhandled_key(key):
+        if key == 'esc':
+            raise urwid.ExitMainLoop()
+        elif key == 'f2':
+            on_save_clicked(None)
+
+    def on_save_clicked(button):
+        count = 0
+        for pile in tlb.original_widget.body:
+            acc_name = pile.contents[1][0].get_edit_text()
+            try:
+                transactions[count]['account'] = options[acc_name]
+            except Exception as e:
+                print("Can't match accounts, please fix: %s" %e)
+                break
+            else:
+                count += 1
+
+        print count, len(transactions)
+        if count == len(transactions):
+            out.release_stdout()
+            raise urwid.ExitMainLoop()
+        else:
+            print("ERROR: not all transactions are matched")
+
+    cancel_b = urwid.Button('ESC:Cancel')
+    save_b = urwid.Button('F2:Save&Exit')
+    urwid.connect_signal(save_b, 'click', on_save_clicked)
+    urwid.connect_signal(cancel_b, 'click', on_cancel_clicked)
+    footer = urwid.AttrMap(urwid.Columns(((18, save_b), (16,cancel_b)),dividechars=5), 'heading')
+    header = urwid.AttrMap(urwid.Text("Edit transactions"), 'heading')
+
+    tlb = urwid.AttrMap(TransactionListBox(transactions, options.keys()), 'selected')
+    acc_options = urwid.AttrMap(urwid.LineBox(urwid.Text("\n".join(options)), title='options'), 'focus options')
+    out = UrwStdWriter()
+
+    debug = urwid.Filler(urwid.LineBox(out.get_widget(), title='debug'), height='flow', min_height=10)
+    frame = urwid.Frame(urwid.Columns((tlb, (20, urwid.Filler(acc_options)))), header=header, footer=footer)
+    loop = urwid.MainLoop(urwid.Pile((('weight', 10, frame), (10, debug))), unhandled_input=unhandled_key, palette=palette)
+    out.capture_stdout()
+    loop.run()
+    print("urwid finished")
+    return transactions
+
+if __name__ == '__main__':
+    gnc_urw_edit(transactions, options)
